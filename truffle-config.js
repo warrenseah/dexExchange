f736418a74b52771af5cd7bc0e12d9daf25ba2b8
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2021-08-26 17:10:54 +0800

    Amend compiled contract in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "clients/src/contracts"),
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   compilers: {
     solc: {
       version: '0.6.3'
